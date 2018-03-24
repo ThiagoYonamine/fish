7065c1094329c0638f9eca4fc921469c1f0cd49f
ThiagoYonamine <thyonamine1@gmail.com>
2018-03-24 17:45:02 -0300

    request image

---
+++ Assets/Scripts/getImages.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class getImages : MonoBehaviour {
+	// The output of the image
+	public Image img;
+
+	// The source image
+	private string url = "http://www.hazteveg.com/img/icons/food/strawberry-icon.png";
+
+	IEnumerator Start() {
+		WWW www = new WWW(url);
+		yield return www;
+		// Create a texture in DXT1 format
+		Texture2D texture = new Texture2D(www.texture.width, www.texture.height, TextureFormat.DXT1, false);
+		// assign the downloaded image to sprite
+		www.LoadImageIntoTexture(texture);
+		Rect rec = new Rect(0, 0, texture.width, texture.height);
+		Sprite spriteToUse = Sprite.Create(texture,rec,new Vector2(0.5f,0.5f),100);
+		img.sprite = spriteToUse;
+		www.Dispose ();
+		www = null;
+	}
+
+}
